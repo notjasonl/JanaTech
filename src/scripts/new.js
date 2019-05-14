7c4918d63c8818334a9cbb526894f53ff384f555
jazon-liu <shadowbox826@gmail.com>
2019-05-14 09:34:35 -0400

    Add code to upload files

--- src/scripts/new.js
+++ src/scripts/new.js
@@ -1,1 +1,14 @@
-const ipc = electron.ipcRenderer
+const ipc = require('electron').ipcRenderer
+const fs = require('graceful-fs')
+
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+function handleFileSelect(evt) {
+    var files = evt.target.files;
+
+    var output = [];
+    for (var i = 0, f; f = files[i]; i++) {
+      output.push('<li class="list-group-item"><strong>', f.name, '</strong></li>');
+    }
+    document.getElementById('list').innerHTML = '<ul class="list-group">' + output.join('') + '</ul>';
+  }
